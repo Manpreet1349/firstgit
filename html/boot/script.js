c32b45bfd7e21711acef3778215f1ee87bce9530
Manpreet1349 <harmansohal470@gmail.com>
2022-02-26 10:43:52 -0500

    commit by masood

---
+++ html/boot/script.js
@@ -0,0 +1,11 @@
+
+//var tablenum =3;
+//for(i=1;i<=10;i++)
+//{
+//    document.writeln(tablenum+"*" + i +"="+tablenum* i+"<br>");
+//}
+var numbers=[23,56,89,96,45,59,-6 ];
+for(i=numbers.length-1;i>0;i--)
+{
+  document.writeln(numbers[i]+"<br>");
+}
