8335660aa8f806bbd41267958c35c45cadd8def3
Manpreet1349 <harmansohal470@gmail.com>
2022-03-12 11:07:06 -0500

    Adding Jquery work

--- html/boot/jquery.js
+++ html/boot/jquery.js
@@ -6,5 +6,8 @@ var txtValue=$('.txt').val();
 $('#list').append("<li>" +txtValue+"</li>");
 $('.txt').val('');
 })
+
+$('input[ type=Text]').val('manpreet');
+$('input[ type=password]').css('background-color','blue');
 }
 )
