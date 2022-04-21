c4e416b93a11a8758eb8cf707c71727751e7f735
Manpreet1349 <harmansohal470@gmail.com>
2022-04-21 16:49:02 -0400

    Added my project

--- html/boot/jquery.js
+++ html/boot/jquery.js
@@ -11,6 +11,24 @@ $(document).ready(function()
 //$('input[ type=text]').val('manpreet');
 //$('input[ type=email]').css('background-color','blue');
 //})
-
-$('input').css('background-color','blue');
 })
+$('input').css('background-color','blue');
+$('.circle').click(function()
+{
+if($(this).hasClass('bg-primary')==true)
+{
+    $('body').addClass('bg-primary');
+}
+else if($(this).hasClass('bg-danger')==true)
+{
+    $('body').addClass('bg-danger');
+}
+else if($(this).hasClass('bg-warning')==true)
+{
+    $('body').addClass('bg-warning');
+}
+else if($(this).hasClass('bg-sucess')==true)
+{
+    $('body').addClass('bg-sucess');
+}
+)
